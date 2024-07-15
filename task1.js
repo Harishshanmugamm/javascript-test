33872e6b83b2137e43bfef1eed116d4d1dad81a9
Harishshanmugamm <harishshanmugamm@outlook.com>
2024-07-15 16:13:49 +0530

    Second Question

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 /*Question 1: 
 Write a program that prints the numbers from 1 to 100. But for multiples of three, print "Fizz" instead of the number and for the 
 multiples of five, print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".*/
-
+/*
 for(let i=1;i<=100;i++){
     if(i%3==0 && i%5==0){
         console.log("FizzBuzz");
@@ -15,4 +15,23 @@ for(let i=1;i<=100;i++){
     else{
         console.log(i)
     }
+}*/
+
+/*2.Write a function that checks if a given string is a palindrome (reads the same forwards and backwards).*/
+/*
+
+function palindrome(x){
+    let y=x.split("").reverse().join("");
+    if(x==y)
+        {
+            console.log("Palindrome")
+        }
+        else{
+            console.log("Not a palindrome")
+        }
 }
+let x="maaam"
+let m="Hello Everyone"
+palindrome(x)
+palindrome(m)
+*/
